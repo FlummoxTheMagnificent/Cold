41744d770856c101af4b27b174e21649a4666bce
FlummoxTheMagnificent <162474077+FlummoxTheMagnificent@users.noreply.github.com>
2024-03-21 14:26:30 +0000

    Shunting yard algorithm with support for negatives

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type keyword struct {
 	key string
 }
 
-func lexer(txt string) ([][]any, []int) {
+func lex(txt string) ([][]any, []int) {
 	var indents []int
 
 	// Split by newlines
@@ -132,11 +132,132 @@ func lexer(txt string) ([][]any, []int) {
 	return lexed, indents
 }
 
+type expression struct {
+	first  any
+	expr   string
+	second any
+}
+
+func shuntingyard(tokens []any, line int) []any {
+	var output []any
+	var queue []any
+
+	isexpr := true
+	negate := false
+	for _, i := range tokens {
+		if typeof(i) == "int" || typeof(i) == "float64" || typeof(i) == "string" {
+			if !(isexpr || (output == nil && queue == nil)) {
+				fmt.Println("Error: unexpected", i, "on line", line)
+				os.Exit(1)
+			}
+			if negate {
+				if typeof(i) == "string" {
+					fmt.Println("Error: cannot negate", i, "on line", line)
+					os.Exit(1)
+				} else {
+					output = append(output, -i.(int))
+				}
+				negate = false
+			} else {
+				output = append(output, i)
+			}
+			isexpr = false
+		} else if i.(keyword).key == "," {
+			for len(queue) > 0 && queue[len(queue)-1] != "(" {
+				output = append(output, queue[len(queue)-1])
+				queue = queue[:len(queue)-1]
+			}
+		} else if i.(keyword).key == "(" {
+			queue = append(queue, i)
+		} else if i.(keyword).key == ")" {
+			for {
+				if len(queue) == 1 && queue[0].(keyword).key != "(" {
+					fmt.Println("Error: missing ( on line", line+1)
+					os.Exit(1)
+				} else if queue[len(queue)-1].(keyword).key == "(" {
+					queue = queue[:len(queue)-1]
+					break
+				}
+				output = append(output, queue[len(queue)-1])
+				queue = queue[:len(queue)-1]
+			}
+		} else if i.(keyword).key == "+" || i.(keyword).key == "-" {
+			if isexpr {
+				if i.(keyword).key == "-" && !negate {
+					negate = true
+					continue
+				} else {
+					fmt.Println("Error: unexpected", i, "(expected value) on line", line+1)
+					os.Exit(1)
+				}
+			}
+			for len(queue) > 0 && (queue[len(queue)-1].(keyword).key == "*" || queue[len(queue)-1].(keyword).key == "/" || queue[len(queue)-1].(keyword).key == "+" || queue[len(queue)-1].(keyword).key == "-") {
+				output = append(output, queue[len(queue)-1])
+				queue = queue[:len(queue)-1]
+			}
+			queue = append(queue, i)
+			isexpr = true
+		} else if i.(keyword).key == "*" || i.(keyword).key == "/" {
+			if isexpr {
+				fmt.Println("Error: unexpected", i, "(expected value) on line", line+1)
+			}
+			for len(queue) > 0 && (queue[len(queue)-1].(keyword).key == "*" || queue[len(queue)-1].(keyword).key == "/") {
+				output = append(output, queue[len(queue)-1])
+				queue = queue[:len(queue)-1]
+			}
+			queue = append(queue, i)
+			isexpr = true
+		} else {
+			if !(isexpr || (output == nil && queue == nil)) {
+				fmt.Println("Error: unexpected", i, "(expected expression) on line", line)
+				os.Exit(1)
+			}
+			output = append(output, i)
+			isexpr = false
+		}
+	}
+
+	for i := len(queue) - 1; i > -1; i-- {
+		if queue[i].(keyword).key == "(" {
+			fmt.Println("Error: missing ) on line", line+1)
+			os.Exit(1)
+		}
+		output = append(output, queue[i])
+	}
+	return output
+}
+
+func parse(program [][]any, _ []int) []any {
+	var lines []any
+	for i, line := range program {
+		line = shuntingyard(line, i)
+		var values []any
+		for _, x := range line {
+			if typeof(x) == "main.keyword" {
+				key := x.(keyword).key
+				if key == "+" || key == "-" || key == "*" || key == "/" {
+					if len(values) < 2 {
+						fmt.Println("Error: missing argument for", key, "on line", i+1)
+						os.Exit(1)
+					}
+					values = append(values[:len(values)-2], expression{values[len(values)-2], key, values[len(values)-1]})
+				} else {
+					values = append(values, key)
+				}
+			} else {
+				values = append(values, x)
+			}
+		}
+		lines = append(lines, values)
+	}
+	return lines
+}
+
 func main() {
 	file := os.Args[1]
 	contentsByteArray, _ := os.ReadFile(file)
 	contents := string(contentsByteArray)
-	lexed, indents := lexer(contents)
-	fmt.Println(lexed)
+	lexed, indents := lex(contents)
+	fmt.Println(parse(lexed, indents)...)
 	fmt.Println(indents)
 }
